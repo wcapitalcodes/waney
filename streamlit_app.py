4ab043403acfd00471e92baf41ae63024c305df5
Uzzair Baharudin <uzzairwebstudio@gmail.com>
2024-05-30 22:43:25 +0800

    change Waney to Wani

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 from llama_index.llms.openai import OpenAI
+from st_chat_message import message
 
 try:
     from llama_index import (
@@ -19,7 +20,7 @@ except ImportError:
 
 
 st.set_page_config(
-    page_title="Chat with Waney. Answer all your questions related to Wahine Capital and finance.",
+    page_title="Chat with Wani. Answer all your questions related to Wahine Capital, women and finance.",
     page_icon="",
     layout="centered",
     initial_sidebar_state="auto",
@@ -29,6 +30,10 @@ openai.api_key = st.secrets.openai_key
 
 gradient_text_html = """
 <style>
+ .st-emotion-cache-1c7y2kd {
+                flex-direction: row-reverse;
+                text-align: right;
+            }
 .gradient-text {
     font-weight: bold;
     background: -webkit-linear-gradient(left, #8e1246, #047481);
@@ -39,16 +44,20 @@ gradient_text_html = """
     font-size: 3em;
 }
 </style>
-<div class="" style="display: flex;align-items:center;"><img src="https://assets.lifeofw.com/waney.png" width="80px" style="margin-right:10px;" /><h5 class="gradient-text">Waney</h5></div>
+<div style="display: flex;align-items:center;margin-bottom:50px;"><img src="https://assets.lifeofw.com/waney.png" width="80px" style="margin-right:10px;" /><div class="flex flex-col"><h5 class="gradient-text">Wani</h5></div></div>
 """
 
-st.markdown(gradient_text_html, unsafe_allow_html=True)
 
-if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+st.markdown(gradient_text_html, unsafe_allow_html=True)
+avatar_url = "https://assets.lifeofw.com/waney.png"
+
+# Initialize the chat messages history
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Hi there! I'm Waney. I'm here to help with any questions you have about Wahine Capital, women, and finance. 😊",
+            "content": "Hi there! I'm Wani. I'm here to help with any questions you have about Wahine Capital, women, and finance. 😊",
+            "avatar": avatar_url,
         }
     ]
 
@@ -60,11 +69,9 @@ def load_data():
     ):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        # llm = OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert o$
-        # index = VectorStoreIndex.from_documents(docs)
         service_context = ServiceContext.from_defaults(
             llm=OpenAI(
-                model="gpt-4o",
+                model="gpt-4",
                 temperature=0.5,
                 system_prompt="Adopt the persona of 'Waney.' Waney is a knowledgeable, supportive, and friendly woman who answers inquiries related to Wahine Capital, women, and finance topics. She responds in an informative, accurate, and empathetic manner, fostering confidence, skills, and financial acumen among women. Waney only answers questions related to finance and Wahine Capital and responds in the user's language for clarity and comfort.For all recommendations, Waney includes a disclaimer that this is not financial advice and users should seek further knowledge or contact Wahine Capital at hellowahine@wcapital.asia.",
             )
@@ -75,25 +82,31 @@ def load_data():
 
 index = load_data()
 
-if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+# Initialize the chat engine
+if "chat_engine" not in st.session_state.keys():
     st.session_state.chat_engine = index.as_chat_engine(
         chat_mode="condense_question", verbose=True
     )
+# Prompt for user input and save to chat history
+if prompt := st.chat_input("Your question"):
+    st.session_state.messages.append(
+        {"role": "user", "content": prompt, "is_user": True}
+    )
 
-if prompt := st.chat_input(
-    "Your question"
-):  # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-for message in st.session_state.messages:  # Display the prior chat messages
-    with st.chat_message(message["role"]):
+# Display the prior chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"], avatar=message.get("avatar", None)):
         st.write(message["content"])
 
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+    with st.chat_message("assistant", avatar=avatar_url):
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(prompt)
             st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
+            message = {
+                "role": "assistant",
+                "content": response.response,
+                "avatar": avatar_url,
+            }
             st.session_state.messages.append(message)  # Add response to message history
