4c4caf3acab1754fc5533dc58d42e4195dc7d41f
Uzzair Baharudin <uzzairwebstudio@gmail.com>
2024-06-04 15:08:58 +0800

    change: put subtitles under Chat With Wani title

--- Wani_Chat.py
+++ Wani_Chat.py
@@ -28,7 +28,7 @@ st.set_page_config(
     menu_items=None,
 )
 st.sidebar.header("Chat with Wani")
-st.sidebar.write("Chat with Wani. Answer all your questions related to Wahine Capital, women and finance.")
+st.sidebar.write("Chat with Wani. Answer all your questions related to Wahine Capital, women and finance. \n Whatever Wani says is not financial advice, and users should seek further knowledge through a financial advisor. Contact Wahine Capital at hellowahine@wcapital.asia or ask Wahine Experts at https://wahine.wcapital.asia/ask.")
 openai.api_key = st.secrets.openai_key
 
 gradient_text_html = """
@@ -47,7 +47,7 @@ gradient_text_html = """
     font-size: 3em;
 }
 </style>
-<div style="display: flex;align-items:center;margin-bottom:50px;"><img src="https://assets.lifeofw.com/waney.png" width="80px" style="margin-right:10px;" /><div class="flex flex-col"><h5 class="gradient-text">Chat with Wani</h5></div></div>
+<div style="display: flex;align-items:center;margin-bottom:50px;"><img src="https://assets.lifeofw.com/waney.png" width="80px" style="margin-right:10px;" /><div class="flex flex-col"><h5 class="gradient-text">Chat with Wani</h5><h6>Wani will answer all your questions related to Wahine Capital, W Vault, Women and Finance. Whatever Wani says is not financial advice, and users should seek further knowledge through a financial advisor..</h6></div></div>
 """
 
 
@@ -60,7 +60,7 @@ if "messages" not in st.session_state.keys():
     st.session_state.messages = [
         {
             "role": "assistant",
-            "content": "Hi there! I'm Wani, your friendly assistant bot here to help with any questions you have about Wahine Capital, Women, and Finance. Just a quick note: I'm still in Beta, so please avoid sharing any confidential or sensitive information during our chat. Thanks! 😊",
+            "content": "Hi there! I'm Wani, your friendly assistant bot here to help with any questions you have about Wahine Capital, W Vault, Women, and Finance. Just a quick note: I'm still in Beta, so please avoid sharing any confidential or sensitive information during our chat. Thanks! 😊",
             "avatar": avatar_url,
         }
     ]
@@ -75,9 +75,9 @@ def load_data():
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(
             llm=OpenAI(
-                model="gpt-4",
+                model="gpt-4o",
                 temperature=0.5,
-                system_prompt="Adopt the persona of 'Wani.' Wani is a knowledgeable, supportive, and friendly woman who answers inquiries related to Wahine Capital, women, and finance topics. She responds in an informative, accurate, and empathetic manner, fostering confidence, skills, and financial acumen among women. Wani only answers questions related to women,finance and Wahine Capital and responds in the user's language for clarity and comfort.For all recommendations,Wani includes a disclaimer that this is not financial advice and users should seek further knowledge through financial advisor, contact Wahine Capital at hellowahine@wcapital.asia or ask Wahine Experts at https://wahine.wcapital.asia/ask",
+                system_prompt="Adopt the persona of 'Wani,' a knowledgeable, supportive, and friendly woman who answers inquiries related to Wahine Capital, W Vault, Women, and Finance topics. Respond informatively, accurately, and empathetically, fostering confidence and financial skills among women. Only answer questions related to these topics. Always include a disclaimer for financial advice: 'This is not financial advice; seek further knowledge through a financial advisor. Contact Wahine Capital at hellowahine@wcapital.asia or ask Wahine Experts at https://wahine.wcapital.asia/ask.' Respond in the user's language for clarity.",
             )
         )
         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
@@ -91,6 +91,45 @@ if "chat_engine" not in st.session_state.keys():
     st.session_state.chat_engine = index.as_chat_engine(
         chat_mode="condense_question", verbose=True
     )
+
+col1, col2= st.columns(2)
+with col1:
+    if st.button("Who is Wahine Capital?",use_container_width=True):
+        question = "Who is Wahine Capital?"
+        st.session_state.messages.append(
+            {"role": "user", "content": question, "is_user": True}
+        )
+with col2:
+    if st.button("What is a Digital Vault?",use_container_width=True):
+        question = "What is Digital Vault?"
+        st.session_state.messages.append(
+            {"role": "user", "content": question, "is_user": True}
+        )
+col3, col4 = st.columns(2)
+with col3:
+    if st.button("What is W Vault?",use_container_width=True):
+        question = "What is W Vault?"
+        st.session_state.messages.append(
+            {"role": "user", "content": question, "is_user": True}
+        )
+with col4:
+    if st.button("What is Notifier List?",use_container_width=True):
+        question = "What is Notifier List?"
+        st.session_state.messages.append(
+            {"role": "user", "content": question, "is_user": True}
+        )
+
+col5, col6 = st.columns(2)
+
+with col5:
+     if st.button("What is Access List?",use_container_width=True):
+        question = "What is Access List?"
+        st.session_state.messages.append(
+            {"role": "user", "content": question, "is_user": True}
+        )
+
+
+
 # Prompt for user input and save to chat history
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append(
@@ -106,7 +145,7 @@ for message in st.session_state.messages:
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant", avatar=avatar_url):
         with st.spinner("Thinking..."):
-            response = st.session_state.chat_engine.chat(prompt)
+            response = st.session_state.chat_engine.chat(st.session_state.messages[-1]["content"])
             st.write(response.response)
             message = {
                 "role": "assistant",
